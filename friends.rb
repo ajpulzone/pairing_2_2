3b0f4499759a5cb8301c4b7ec3e183a91bf66935
Annie Pulzone <ajpulzone@gmail.com>
2022-07-17 18:04:21 -0700

    Add locations

--- friends.rb
+++ friends.rb
@@ -3,3 +3,7 @@ friends = ["Lauren", "Emily", "Zach", "Nick", "Bruce"]
 puts friends [2]
 puts friends[4]
 puts friends[0]
+
+vacation_spots = ["Italy", "California", "Africa", "New Zeland", "France"]
+
+puts vacation_spots.pop
